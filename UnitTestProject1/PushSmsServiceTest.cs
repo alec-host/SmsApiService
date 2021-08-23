c73e33986a07ef65b8a2d0ec408f8423a027c81f
alec-host <irualec@yahoo.com>
2021-08-23 21:10:28 +0300

    6th commit

---
+++ UnitTestProject1/PushSmsServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PUSH_SMS_SERVICE;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class PushSmsServiceTest
+    {
+        [TestMethod]
+        public void AdditionTest()
+        {
+            Program program = new Program();
+
+            int number1 = 10;
+            int number2 = 5;
+
+            int check = program.Addition(number1, number2);
+
+            Assert.AreEqual(15, check);
+        }
+    }
+}
